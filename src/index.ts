6de50db3f480673a49d95a58d047be27085a3866
dianavolt <diana.voltchok@telus.com>
2020-05-31 17:38:12 -0800

    feat: create CustomMap

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,12 @@
 // <reference types="@types/google.maps" />
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
 const company = new Company();
 
+new CustomMap('map');
+
 console.log(user)
 console.log(company)
-
-new google.maps.Map(document.getElementById('map'), {
-	zoom: 1,
-	center: {
-		lat: 0,
-		lng: 0
-	}
-})
