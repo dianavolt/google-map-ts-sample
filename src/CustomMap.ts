026189e4caa540cc41fdabbaf001f1f90d249fb2
dianavolt <diana.voltchok@telus.com>
2020-06-02 19:29:27 -0800

    feat: add marker info window

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -5,7 +5,8 @@ interface Mapable {
 	location: {
 		lat: number,
 		lng: number
-	}
+	};
+	markerContent(): string;
 }
 
 export class CustomMap {
@@ -22,12 +23,20 @@ export class CustomMap {
 	}
 
 	addMarker(mappable: Mapable): void {
-		new google.maps.Marker({
+		const marker = new google.maps.Marker({
 			map: this.googleMap,
 			position: {
 				lat: mappable.location.lat,
 				lng: mappable.location.lng
 			}
+		})
+
+		marker.addListener('click', () => {
+			const infoWindow = new google.maps.InfoWindow({
+				content: mappable.markerContent()
+			})
+
+			infoWindow.open(this.googleMap, marker)
 		})
 	}
 }
